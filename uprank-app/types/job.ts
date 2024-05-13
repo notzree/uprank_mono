5bb737c73a581493e444d06ee7ef0ebdda50c24c
notzree <notzree@gmail.com>
2024-05-12 21:02:30 -0400

    Update types

--- uprank-app/types/job.ts
+++ uprank-app/types/job.ts
@@ -1,13 +1,13 @@
-export interface ScrapedJobData {
+export type ScrapedJobData = {
     id: string;
     title: string;
-    description: string;
     location: string;
+    description: string;
+    skills: string[];
+    experience_level: string;
     hourly: boolean;
     fixed: boolean;
     hourly_rate: number[];
-    fixed_rate: number[];
-    num_months: number[];
-    experience_level: string;
-    skills: string[];
-}
+    fixed_rate: number;
+  }
+  
--- uprank-extension/src/types/job.ts
+++ uprank-extension/src/types/job.ts
@@ -10,25 +10,34 @@ export type Job = {
     hourly: boolean;
     fixed: boolean;
     hourlyRate: number[];
-    fixedRate: number[];
-    numMonths: number[];
+    fixedRate: number;
     averageUprankScore: number | null;
     maxUprankScore: number | null;
     minUprankScore: number | null;
 
   };
 
+  //Type that gets sent from content -> popup
   export type ScrapedJobData = {
     id: string;
     title: string;
     location: string;
     description: string;
     skills: string[];
-    experienceLevel: string;
+    experience_level: string;
     hourly: boolean;
     fixed: boolean;
-    hourlyRate: number[];
-    fixedRate: number;
-    numMonths: number[];
+    hourly_rate: number[];
+    fixed_rate: number;
+  }
+
+  export type sendJobBody = {
+    authentication_token: string;
+    job: ScrapedJobData;
+  }
+
+  export type UnstableScrapedJobData = {
+    job: ScrapedJobData;
+    missingFields: boolean;
   }
   
