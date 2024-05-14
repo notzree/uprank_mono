5834f5da6bec2d6ac8d48ef9ef1b4cd04684baa5
notzree <notzree@gmail.com>
2024-05-13 22:05:18 -0400

    Background SW to send freelancers to backend

--- uprank-extension/src/background/messages/send-freelancers.ts
+++ uprank-extension/src/background/messages/send-freelancers.ts
@@ -1,25 +1,29 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
-import type { ScrapedFreelancerData, sendFreelancerBody} from "~types/freelancer"
+import type { ScrapedFreelancerData, SendFreelancerBody, SendFreelancerResponse} from "~types/freelancer"
  
 //todo: Implement this, currently is just copy pasated over from the send-jobs file.
 //then need to create the backend for this endpoint.
-const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-    const body: sendFreelancerBody = req.body;
+const handler: PlasmoMessaging.MessageHandler = async (req, res)   => {
+    const body: SendFreelancerBody = req.body;
     const response = await fetch(
-      `${process.env.PLASMO_PUBLIC_BACKEND_URL}/api/private/freelancer`,
+      `${process.env.PLASMO_PUBLIC_BACKEND_URL}/api/private/freelancer/${body.jobId}/addFreelancers`,
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${body.authentication_token}`
         },
-        body: JSON.stringify(body.freelancer)
+        body: JSON.stringify(body.freelancers)
       }
     )
+    const response_json = await response.json()
     res.send({
-      status: response.ok
-    })
+      ok: response.ok,
+      count: response_json?.count,
+    } as SendFreelancerResponse)
     return;
 }
  
 export default handler
+
+
