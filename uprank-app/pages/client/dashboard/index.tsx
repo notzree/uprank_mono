a190e2a1a5b5f40b9cbaff212ee0eecc56ea25cd
notzree <notzree@gmail.com>
2024-05-15 09:08:24 -0400

    fetch and render jobs

--- uprank-app/pages/client/dashboard/index.tsx
+++ uprank-app/pages/client/dashboard/index.tsx
@@ -6,13 +6,11 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import { FreelancerList } from "./components/freelancer-list";
 import Navbar from "./components/navbar";
 import { serializeDates } from "@/utils/api_utils/serialize";
 
-export default function Dashboard({
-    freelancer_props,
-}: any) {
+export default function Dashboard({ jobs }: any) {
+    console.log(jobs);
     return (
         <div className="grid min-h-screen w-full">
             <div className="flex flex-col">
@@ -20,19 +18,30 @@ export default function Dashboard({
                 <main className="flex flex-1 flex-col items-center justify-center gap-4 p-4 md:gap-8 md:p-6">
                     <div className="flex flex-row items-center justify-center w-full text-center">
                         <div className=" flex flex-col flex-1">
-                        <h2>
-                            Your current job postings
-                        </h2>
+                            <h2>Your current job postings</h2>
+                            <div className="flex-1">
+                            {
+                                jobs.map((job: any) => {
+                                    return (
+                                        <div key={job.id} className="flex flex-row justify-between">
+                                            <div>
+                                                <h3>{job.title}</h3>
+                                                <p>{job.created_at}</p>
+                                            </div>
+                                            <div>
+                                                <button>Edit</button>
+                                                <button>Delete</button>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                            </div>
+                        </div>
                         <div className="flex-1">
-                        Render the jobs here (Fetched from upwork api)
-                        </div>
-                        </div>
-                        <div className= "flex-1">
-                            <h2>
-                                Your former Upranks
-                            </h2>
+                            <h2>Your former Upranks</h2>
                             <div>
-                                Render the former upranks here (Fetched from our database, but restricted to only storing certain data as upwork does not allow caching beyond 24 hours.)
+                                
                             </div>
                         </div>
                     </div>
@@ -43,3 +52,33 @@ export default function Dashboard({
 }
 
 
+
+import { getAuth } from "@clerk/nextjs/server";
+import { GetServerSideProps } from "next";
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+    const { userId } = getAuth(ctx.req);
+    if (!userId) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    const jobs = await prisma.job.findMany({
+        where: {
+            user_id: userId,
+        },
+    });
+    jobs.forEach((job) => {
+        job = serializeDates(job);
+    });
+
+    return {
+        props: {
+            jobs: jobs,
+        },
+    };
+}
