32342da40de994295f4cddb4974cce6daacc7f36
notzree <notzree@gmail.com>
2024-05-21 18:53:33 -0400

    Implement CreateJob API Handler

--- uprank-backend/main-backend/api/job_handlers.go
+++ uprank-backend/main-backend/api/job_handlers.go
@@ -1,1 +1,37 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/notzree/uprank-backend/main-backend/types"
+)
+
+func (s *Server) CreateJob(w http.ResponseWriter, r *http.Request) error {
+	var req types.CreateJobRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return InvalidJSON()
+	}
+
+	defer r.Body.Close()
+	if errors := req.Validate(); len(errors) > 0 {
+		return InvalidRequestData(errors)
+	}
+	new_user, err := s.ent.Job.Create().
+		SetID(req.Id).
+		SetTitle(req.Title).
+		SetLocation(req.Location).
+		SetDescription(req.Description).
+		SetSkills(req.Skills).
+		SetExperienceLevel(req.Experience_level).
+		SetHourly(req.Hourly).
+		SetFixed(req.Fixed).
+		SetHourlyRate(req.Hourly_rate).
+		SetFixedRate(req.Fixed_rate).
+		Save(r.Context())
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, http.StatusCreated, new_user)
+}
