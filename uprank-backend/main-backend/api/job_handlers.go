19770cf1f8dd072b6d33621be7d4901cc95a4b4e
notzree <notzree@gmail.com>
2024-05-21 18:58:45 -0400

    reminder 2 myself

--- uprank-backend/main-backend/api/job_handlers.go
+++ uprank-backend/main-backend/api/job_handlers.go
@@ -18,7 +18,9 @@ func (s *Server) CreateJob(w http.ResponseWriter, r *http.Request) error {
 	if errors := req.Validate(); len(errors) > 0 {
 		return InvalidRequestData(errors)
 	}
-	new_user, err := s.ent.Job.Create().
+	//THIS ACTUALLY WON'T CREATE A JOB PROEPRLY BECAUSE IT NEEDS TO BE ASSOCIATED WITH A USER BEFORE BEING CREATED.
+	//NEED TO USE CLERK TO FETCH USER ID AND THEN MAKE AN EDGE WITH A USER
+	new_job, err := s.ent.Job.Create().
 		SetID(req.Id).
 		SetTitle(req.Title).
 		SetLocation(req.Location).
@@ -29,9 +31,10 @@ func (s *Server) CreateJob(w http.ResponseWriter, r *http.Request) error {
 		SetFixed(req.Fixed).
 		SetHourlyRate(req.Hourly_rate).
 		SetFixedRate(req.Fixed_rate).
+		SetUserID("FIX THIS SHIT YO ").
 		Save(r.Context())
 	if err != nil {
 		return err
 	}
-	return writeJSON(w, http.StatusCreated, new_user)
+	return writeJSON(w, http.StatusCreated, new_job)
 }
