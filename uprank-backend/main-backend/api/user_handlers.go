b509af1050ac1b470efe0dc674a4aa3eb27c95c3
notzree <notzree@gmail.com>
2024-05-21 12:06:56 -0400

    Add UpdateUser API

--- uprank-backend/main-backend/api/user_handlers.go
+++ uprank-backend/main-backend/api/user_handlers.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+	"time"
 
 	"github.com/notzree/uprank-backend/main-backend/types"
 )
@@ -11,6 +11,7 @@ import (
 // Creates a user in the database after they have signed up w/ clerk + completed onboarding
 func (s *Server) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	var req types.CreateUserRequest
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		return InvalidJSON()
 	}
@@ -19,18 +20,35 @@ func (s *Server) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	if errors := req.Validate(); len(errors) > 0 {
 		return InvalidRequestData(errors)
 	}
-	log.Printf("Creating user")
 	new_user, err := s.ent.User.Create().
 		SetID(req.User.ID).
 		SetFirstName(req.User.FirstName).
 		SetCompanyName(req.User.CompanyName).
 		SetEmail(req.User.Email).
 		Save(r.Context())
-	log.Printf("Finished creating user")
 	if err != nil {
 		return err
 	}
-
 	return writeJSON(w, http.StatusCreated, new_user)
+}
 
+func (s *Server) UpdateUser(w http.ResponseWriter, r *http.Request) error {
+	//Clerk sends the request via webhooks so the body is guaranteed to be well formatted
+	var req types.UpdateUserRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return InvalidJSON()
+	}
+	defer r.Body.Close()
+
+	updated_user, err := s.ent.User.
+		UpdateOneID(req.ClerkUserData.ID).
+		SetFirstName(req.ClerkUserData.FirstName).
+		SetLastLogin(time.Unix(req.ClerkUserData.LastSignInAt, 0)). //Convert timestamp into time.Time
+		SetUpdatedAt(time.Unix(req.ClerkUserData.UpdatedAt, 0)).
+		Save(r.Context())
+
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, http.StatusOK, updated_user)
 }
--- uprank-backend/main-backend/types/user_types.go
+++ uprank-backend/main-backend/types/user_types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"strings"
+	"time"
 
 	"github.com/notzree/uprank-backend/main-backend/ent"
 )
@@ -34,4 +35,55 @@ func (req *CreateUserRequest) Validate() map[string]string {
 	}
 
 	return errors
+}
+
+type UpdateUserRequest struct {
+	ClerkUserData ClerkUserData `json:"data"`
+	Object        string        `json:"object"`
+	Type          string        `json:"type"`
+}
+type ClerkUserData struct {
+	Birthday              string                 `json:"birthday"`
+	CreatedAt             int64                  `json:"created_at"`
+	EmailAddresses        []EmailAddress         `json:"email_addresses"`
+	ExternalAccounts      []interface{}          `json:"external_accounts"`
+	ExternalID            interface{}            `json:"external_id"`
+	FirstName             string                 `json:"first_name"`
+	Gender                string                 `json:"gender"`
+	ID                    string                 `json:"id"`
+	ImageURL              string                 `json:"image_url"`
+	LastName              *string                `json:"last_name"`
+	LastSignInAt          int64                  `json:"last_sign_in_at"`
+	Object                string                 `json:"object"`
+	PasswordEnabled       bool                   `json:"password_enabled"`
+	PhoneNumbers          []interface{}          `json:"phone_numbers"`
+	PrimaryEmailAddressID string                 `json:"primary_email_address_id"`
+	PrimaryPhoneNumberID  *string                `json:"primary_phone_number_id"`
+	PrimaryWeb3WalletID   *string                `json:"primary_web3_wallet_id"`
+	PrivateMetadata       map[string]interface{} `json:"private_metadata"`
+	ProfileImageURL       string                 `json:"profile_image_url"`
+	PublicMetadata        map[string]interface{} `json:"public_metadata"`
+	TwoFactorEnabled      bool                   `json:"two_factor_enabled"`
+	UnsafeMetadata        map[string]interface{} `json:"unsafe_metadata"`
+	UpdatedAt             int64                  `json:"updated_at"`
+	Username              *string                `json:"username"`
+	Web3Wallets           []interface{}          `json:"web3_wallets"`
+}
+
+// EmailAddress represents the email address structure
+type EmailAddress struct {
+	EmailAddress string        `json:"email_address"`
+	ID           string        `json:"id"`
+	LinkedTo     []interface{} `json:"linked_to"`
+	Object       string        `json:"object"`
+	Reserved     bool          `json:"reserved"`
+	Verification Verification  `json:"verification"`
+}
+
+// Verification represents the verification structure
+type Verification struct {
+	Attempts *int       `json:"attempts"`
+	ExpireAt *time.Time `json:"expire_at"`
+	Status   string     `json:"status"`
+	Strategy string     `json:"strategy"`
 }
