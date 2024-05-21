9ead0a89653cef0e4ce51d2f8abc613fd7af8016
notzree <notzree@gmail.com>
2024-05-21 12:06:23 -0400

    add in clerk middleware

--- uprank-backend/main-backend/api/server.go
+++ uprank-backend/main-backend/api/server.go
@@ -3,36 +3,46 @@ package api
 import (
 	"net/http"
 
+	"github.com/clerk/clerk-sdk-go/v2"
+	clerkhttp "github.com/clerk/clerk-sdk-go/v2/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/notzree/uprank-backend/main-backend/ent"
 )
 
 type Server struct {
-	Port   string
-	Router *chi.Mux
-	ent    *ent.Client
-	//remember to add clerk client here or in the middleware
+	Port             string
+	Router           *chi.Mux
+	ent              *ent.Client
+	Clerk_secret_key string
 }
 
-func NewServer(listen_addr string, router *chi.Mux, Db *ent.Client) *Server {
+func NewServer(listen_addr string, router *chi.Mux, ent *ent.Client, clerk_secret_key string) *Server {
 	return &Server{
-		Port:   listen_addr,
-		Router: router,
-		ent:    Db,
+		Port:             listen_addr,
+		Router:           router,
+		ent:              ent,
+		Clerk_secret_key: clerk_secret_key,
 	}
 }
 
 func (s *Server) Start() error {
-	//public apis
-	s.Router.Group(func(r chi.Router) {
-		r.Route("/public", func(r chi.Router) {
-			r.Post("/users", Make(s.CreateUser))
+	clerk.SetKey(s.Clerk_secret_key)
+	s.Router.Route("/v1", func(r chi.Router) {
+		//public apis
+		r.Group(func(r chi.Router) {
+			r.Route("/public", func(r chi.Router) {
+				r.Post("/users", Make(s.CreateUser))
+				r.Post("/users/update", Make(s.UpdateUser))
+			})
+		})
+		//private apis
+		r.Group(func(r chi.Router) {
+			r.Use(clerkhttp.RequireHeaderAuthorization())
+			r.Route("/private", func(r chi.Router) {
+
+			})
 		})
 
-	})
-	//private apis
-	s.Router.Group(func(r chi.Router) {
-		// r.Use(AuthMiddleware)  todo: implement AuthMiddleware
 	})
 	return http.ListenAndServe(s.Port, s.Router)
 }
