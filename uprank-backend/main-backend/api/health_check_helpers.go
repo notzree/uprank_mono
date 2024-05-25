45dd1332f6dcec27be8fe2f5171d441ebf2c810b
notzree <notzree@gmail.com>
2024-05-24 22:48:13 -0400

    Add basic healthcheck logic

---
+++ uprank-backend/main-backend/api/health_check_helpers.go
@@ -0,0 +1,20 @@
+package api
+
+import "net/http"
+
+func (s *Server) HealthCheck(w http.ResponseWriter, r *http.Request) error {
+	if clerkErr := checkClerk(); clerkErr != nil {
+		return clerkErr
+	}
+
+	return writeJSON(w, http.StatusOK, "healthy")
+}
+
+func checkClerk() error {
+	_, err := http.Get("https://upright-crow-79.accounts.dev")
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
