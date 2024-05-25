4cf905ae96cefc341311563459957e3c4432908d
notzree <notzree@gmail.com>
2024-05-25 10:08:58 -0400

    Query database to see if successful

--- uprank-backend/main-backend/api/health_check_helpers.go
+++ uprank-backend/main-backend/api/health_check_helpers.go
@@ -3,8 +3,13 @@ package api
 import "net/http"
 
 func (s *Server) HealthCheck(w http.ResponseWriter, r *http.Request) error {
+
 	if clerkErr := checkClerk(); clerkErr != nil {
 		return clerkErr
+	}
+	_, err := s.ent.User.Query().First(r.Context())
+	if err != nil {
+		return err
 	}
 
 	return writeJSON(w, http.StatusOK, "healthy")
