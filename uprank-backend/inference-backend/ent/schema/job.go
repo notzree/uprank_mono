ee4bc2966653d194c92941374975af9d22079a42
notzree <notzree@gmail.com>
2024-06-15 06:39:49 -0400

    copy ent schema for inference server

---
+++ uprank-backend/inference-backend/ent/schema/job.go
@@ -0,0 +1,46 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+type Job struct {
+	ent.Schema
+}
+
+func (Job) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New).
+			StorageKey("oid"),
+		field.Enum("origin_platform").GoType(Platform("")).Immutable(), //Enum for the starting platform type the job was created. Can be created either through Uprank or through extension (fiverr, upwork, etc)
+	}
+}
+
+// Edges of the Job.
+func (Job) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("user", User.Type).
+			Ref("job").
+			Unique().Required(),
+		edge.To("upworkjob", UpworkJob.Type),
+	}
+}
+
+// Enum for platform type
+type Platform string
+
+const (
+	Upwork Platform = "upwork"
+	Uprank Platform = "uprank"
+)
+
+func (Platform) Values() (kinds []string) {
+	for _, s := range []Platform{Upwork, Uprank} {
+		kinds = append(kinds, string(s))
+	}
+	return
+}
