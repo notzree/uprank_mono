0cebdcb4f50ba4e15ae4e745cb83d5bcda504646
notzree <notzree@gmail.com>
2024-06-15 06:41:03 -0400

    Service business logic

---
+++ uprank-backend/inference-backend/service/service.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"context"
+
+	"github.com/notzree/uprank-backend/inference-backend/types"
+)
+
+type Service interface {
+	RankUpworkJob(data types.UpworkRankingMessage, ctx context.Context) (types.UpworkRankingResult, error)
+}
