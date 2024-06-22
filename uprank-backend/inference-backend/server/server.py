7c4dce5b96243cfc984091b69559d0f4d31d66a1
notzree <notzree@gmail.com>
2024-06-21 22:45:08 -0700

    Fix Microservice auth

--- uprank-backend/inference-backend/server/server.py
+++ uprank-backend/inference-backend/server/server.py
@@ -1,6 +1,8 @@
-from queues import Queue
-from service import Service
 import concurrent.futures
+
+from queues import Queue
+from queues.types import RankingRequest
+from service import Service
 
 
 class Server:
@@ -8,11 +10,18 @@ class Server:
         self.queue = queue
         self.max_workers = max_workers
         self.service = service
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers)
+        self.executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=self.max_workers,
+        )
 
-    def start(self):
+    def start(self) -> None:
         while True:
             response = self.queue.poll_ranker_queue()
             if response:
                 for item in response:
-                    self.executor.submit(self.service.rank, item)
+                    self.executor.submit(self.rank_and_delete, item)
+
+    def rank_and_delete(self, item: RankingRequest) -> None:
+        self.service.rank(item)
+        self.queue.delete_message(item.receipt_handle)
+
