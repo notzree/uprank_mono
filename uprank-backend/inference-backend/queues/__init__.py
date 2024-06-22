1ca430cd032ab5307560f4f2dcf6a906fad49e43
notzree <notzree@gmail.com>
2024-06-21 22:44:30 -0700

    Make python microservice

--- uprank-backend/inference-backend/queues/__init__.py
+++ uprank-backend/inference-backend/queues/__init__.py
@@ -1,6 +1,6 @@
 # queue/__init__.py
-from .sqs_queue import SQSQueue
 from .queue import Queue
+from .sqs_queue import SQSQueue
 from .types import RankingRequest
 
-__all__ = ['SQSQueue', 'Queue', "RankingRequest"]
+__all__ = ["SQSQueue", "Queue", "RankingRequest"]
