08383f3081ce0f69e1ab4fbe764aa16c3e31b77f
notzree <notzree@gmail.com>
2024-06-21 22:45:17 -0700

    Add vector stuff

---
+++ uprank-backend/inference-backend/vector/vector.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Vector(ABC):
+
+
+
+
+
+
+# What this class needs to be able to do:
+# 1. Initialize a vector index
+# 2. Embed and add a vector into the index
