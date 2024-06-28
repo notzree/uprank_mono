b395471a59635ae8c9ccfd7e4d1c664bf681b9e7
notzree <notzree@gmail.com>
2024-06-27 22:53:08 -0700

    embedding logic w/ grpc client

--- uprank-backend/queue-handler/main.go
+++ uprank-backend/queue-handler/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	client "github.com/notzree/uprank-backend/queue-handler/grpc_client"
@@ -12,24 +13,18 @@ import (
 
 func main() {
 	ranking_queue_url := os.Getenv("RANKING_QUEUE_URL")
-	grpc_server_url := ""
+	main_backend_url := os.Getenv("MAIN_BACKEND_URL")
+	// inference_server_url := os.Getenv("INFERENCE_SERVER_URL")
+	// log.Default().Print(inference_server_url)
+	inference_server_url := "uprank-inference-backend:50051"
+	ms_api_key := os.Getenv("MS_API_KEY")
 	queue := queue.NewSqsQueue(ranking_queue_url)
-	grpc_inference_client, err := client.NewGRPCInferenceClient(grpc_server_url)
+	grpc_inference_client, err := client.NewGRPCInferenceClient(inference_server_url)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Error creating grpc client: %v", err))
 	}
 
-	server := server.NewServer(ranking_queue_url, queue, grpc_inference_client)
+	client := http.Client{}
+	server := server.NewServer(ranking_queue_url, main_backend_url, queue, grpc_inference_client, ms_api_key, client)
 	server.Start()
 }
-
-//server
-//server -> queue, service
-//queue -> handles polling for messages
-//services -> business logic
-
-// service -> grpc_client -> interact with python server
-
-//server
-//grpc_client
-//queue
