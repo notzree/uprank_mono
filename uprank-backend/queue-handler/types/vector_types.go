03c2158e2eda6b1aaf4eac7d035938b0b31db3c4
notzree <notzree@gmail.com>
2024-06-27 22:46:22 -0700

    update types

--- uprank-backend/queue-handler/types/vector_types.go
+++ uprank-backend/queue-handler/types/vector_types.go
@@ -1,8 +1,15 @@
 package types
 
+import proto "github.com/notzree/uprank-backend/queue-handler/proto"
+
 type Metadata struct {
 	UserID   string `json:"user_id"`
 	JobID    string `json:"job_id"`
 	Type     string `json:"type"`
 	Platform string `json:"platform"`
+}
+
+type UpsertVectorResponse struct {
+	Job_description_vector *proto.Vector `json:"job_description_vector"`
+	Job_skill_vector       *proto.Vector `json:"job_skill_vector"`
 }
