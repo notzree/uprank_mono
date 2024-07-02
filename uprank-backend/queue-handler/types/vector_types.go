3ed0d0299f86a71bfa50463f4e7d09f8ea34562f
notzree <notzree@gmail.com>
2024-07-02 09:11:12 -0700

    typing

--- uprank-backend/queue-handler/types/vector_types.go
+++ uprank-backend/queue-handler/types/vector_types.go
@@ -10,6 +10,14 @@ type Metadata struct {
 }
 
 type UpsertVectorResponse struct {
+	Job_description_vector  *proto.Vector `json:"job_description_vector"`
+	Job_skill_vector        *proto.Vector `json:"job_skill_vector"`
+	Upserted_freelancer_ids []string      `json:"upserted_freelancer_ids"`
+}
+
+type ComputeSpecializationRequest struct {
+	Job_id                 string        `json:"job_id"`
+	Work_history_count     int32         `json:"freelancer_count"`
 	Job_description_vector *proto.Vector `json:"job_description_vector"`
 	Job_skill_vector       *proto.Vector `json:"job_skill_vector"`
 }
