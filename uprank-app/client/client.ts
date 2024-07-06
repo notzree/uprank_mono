278da99bf67c1e489c2d8b709f09294d421ba3d0
notzree <notzree@gmail.com>
2024-07-06 09:32:27 -0700

    rest client changes

--- uprank-app/client/client.ts
+++ uprank-app/client/client.ts
@@ -4,6 +4,8 @@ import {Job} from "@/types/job"
 //The client interacts with the server and handles all the transportation logic of fetching / serializing data
 export interface Client {
     GetAllJobs(auth_token: string): Promise<GetJobResponse>;
+    GetPlatformJobById(job_id: string, auth_token: string): Promise<Job>;
+    Rank(req: RankRequest, auth_token: string): Promise<void>;
 }
 
 export type GetJobResponse = {
@@ -11,3 +13,10 @@ export type GetJobResponse = {
     
 }
 
+
+export type RankRequest = {
+    job_id: string;
+    user_id: string;
+    platform_id: string;
+    platform: string;
+}
--- uprank-app/client/v1_client.ts
+++ uprank-app/client/v1_client.ts
@@ -1,6 +1,5 @@
-import { AnyARecord } from "dns";
-import { Client, GetJobResponse } from "./client";
-
+import { Client, GetJobResponse, RankRequest } from "./client";
+import { Job } from "@/types/job";
 export class v1Client implements Client {
     private base_url: string;
     
@@ -29,4 +28,20 @@ export class v1Client implements Client {
             body: JSON.stringify(body),
         }
     }
+
+    async GetPlatformJobById(job_id: string,auth_token: string): Promise<Job> {
+        const url = this.base_url + "/v1/private/jobs/" + job_id + "/"
+        const response = await fetch(url, this._build_request(auth_token, "GET"));
+        const data = await response.json();
+        return data;
+    }
+
+    async Rank(req: RankRequest, auth_token: string): Promise<void> {
+        const url = this.base_url + "/v1/test";
+        const response = await fetch(url, this._build_request(auth_token, "POST", req));
+        if (!response.ok) {
+            throw new Error("Failed to rank jobs");
+        }
+    }
 }
+
