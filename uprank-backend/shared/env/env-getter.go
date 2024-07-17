a303ed18f4d179c2c175f59e46a4b2144be40697
notzree <notzree@gmail.com>
2024-07-16 20:59:47 -0400

    Add shared environment variable structs

---
+++ uprank-backend/shared/env/env-getter.go
@@ -0,0 +1,44 @@
+package env
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
+type EnvGetter interface {
+	GetEnv(env string) (string, error)
+}
+
+type AwsEnvGetter struct {
+	EnvMap map[string]string
+}
+
+func NewAwsEnvGetter() *AwsEnvGetter {
+	all_secrets := os.Getenv("MAIN_BACKEND_SECRETS")
+	em := make(map[string]string)
+	if err := json.Unmarshal([]byte(all_secrets), &em); err != nil {
+		log.Fatalf("failed to unmarshal secrets: %v", err)
+	}
+	return &AwsEnvGetter{
+		EnvMap: em,
+	}
+}
+
+func (a *AwsEnvGetter) GetEnv(env string) (string, error) {
+	if val, ok := a.EnvMap[env]; ok {
+		return val, nil
+	}
+	return "", errors.New("env not found")
+}
+
+type LocalEnvGetter struct{}
+
+func (l *LocalEnvGetter) GetEnv(env string) (string, error) {
+	val := os.Getenv(env)
+	if val == "" {
+		return "", errors.New("env not found")
+	}
+	return val, nil
+}
