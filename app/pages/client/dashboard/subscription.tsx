486f2c5db297c0d42a0592195575769030aa82b6
notzree <notzree@gmail.com>
2024-07-24 22:39:43 -0400

    sub page

---
+++ app/pages/client/dashboard/subscription.tsx
@@ -0,0 +1,11 @@
+export default function Subscription() {
+    return (
+        <div className="w-screen min-h-screen flex flex-col justify-center items-center">
+            <p className="w-96">
+            Whoops! You&apos;ve caught us with our pants down. This page is still under construction.
+            To be notified when Uprank comes out of beta, please sign up for our mailing list below.
+            </p>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBaHMQRNB_rh3clyM3HzszbRgr9-3Zdk2nOLx8i2UIugRXMQ/viewform?embedded=true" width="640" height="600">Loading…</iframe>
+        </div>
+    )
+}
