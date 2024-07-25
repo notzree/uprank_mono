fbf1a73432cc6183992031d1da6f3355d24d5ab3
notzree <notzree@gmail.com>
2024-07-25 13:19:11 -0400

    raawwr

--- app/pages/client/dashboard/subscription.tsx
+++ app/pages/client/dashboard/subscription.tsx
@@ -4,7 +4,7 @@ export default function Subscription() {
             <p className="w-96">
             Whoops! You&apos;ve caught us with our pants down. This page is still under construction.
             To be notified when Uprank comes out of beta, please sign up for our mailing list below.
-            
+
             </p>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBaHMQRNB_rh3clyM3HzszbRgr9-3Zdk2nOLx8i2UIugRXMQ/viewform?embedded=true" width="640" height="600">Loading…</iframe>
         </div>
