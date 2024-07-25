cef59b8d56690a441fdd3c214bfbfa768ff7de52
notzree <notzree@gmail.com>
2024-07-25 17:16:42 -0400

    merge

--- app/sst.config.ts
+++ app/sst.config.ts
@@ -1,6 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-
 export default $config({
     app(input) {
         return {
@@ -8,21 +7,6 @@ export default $config({
             removal: input?.stage === "production" ? "retain" : "remove",
             home: "aws",
         };
-    },
-    input: {
-        console: {
-            autodeploy: {
-                target(event) {
-                    if (event.type === "branch" && event.branch === "production" && event.action === "pushed") {
-                        return { stage: "production" };
-                    }
-                    // if (event.type ==="branch" && event.branch ==="main" && event.action ==="pushed"){
-                    //     return {stage: "dev"};
-                    // }
-                    return;
-                }
-            }
-        }
     },
     async run() {
         new sst.aws.Nextjs("UprankApp");
